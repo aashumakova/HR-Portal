9c079e49f6ec6e6ce30afdfac0829bfeef117a66
aashumakova <aashumakova@gmail.com>
2023-05-16 12:34:49 -0700

    only logged in can users can manipulate data

--- routes/employees.js
+++ routes/employees.js
@@ -3,13 +3,14 @@ const router = express.Router()
 
 const employeeCtrl = require('../controllers/employees')
 const employee = require('../models/employee')
+const isLoggedIn = require('../config/auth');
 //localhost:3000/employees
 router.get('/', employeeCtrl.index)
-router.get('/new', employeeCtrl.newEmployee)
-router.post('/',employeeCtrl.create)
-router.get('/:id', employeeCtrl.show)
-router.get('/:id/edit', employeeCtrl.updateEmployeeForm)
-router.put('/:id/', employeeCtrl.update)
-router.delete('/:id', employeeCtrl.deleteEmployee)
+router.get('/new', isLoggedIn, employeeCtrl.newEmployee)
+router.post('/', isLoggedIn, employeeCtrl.create)
+router.get('/:id', isLoggedIn, employeeCtrl.show)
+router.get('/:id/edit', isLoggedIn, employeeCtrl.updateEmployeeForm)
+router.put('/:id/', isLoggedIn, employeeCtrl.update)
+router.delete('/:id', isLoggedIn, employeeCtrl.deleteEmployee)
 
 module.exports = router
--- routes/evaluations.js
+++ routes/evaluations.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const evaluationsCtrl = require('../controllers/evaluations');
+const isLoggedIn = require('../config/auth');
 // refer to it as name in show
-router.post('/employees/:id/evaluations', evaluationsCtrl.create);
+router.post('/employees/:id/evaluations', isLoggedIn, evaluationsCtrl.create);
 
 
 module.exports = router
